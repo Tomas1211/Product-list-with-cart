992928a005a97e7a4dc86377294da69fb3f6162a
Tomáš Modrovský <modrovskytomas@gmail.com>
2026-01-10 09:23:05 +0100

    Adding div.wrapper around the picture element + placing the button inside of it for better positioning

--- js/script.js
+++ js/script.js
@@ -14,17 +14,19 @@ fetch("./data.json")
       li.className = "dessert"
       li.innerHTML = 
       `
-        <picture class="dessert__image">
-          <source media="(min-width:1024px)" srcset="${item.image.desktop}">
-          <source media="(min-width:760px)" srcset="${item.image.tablet}">
-          <img src="${item.image.mobile}">
-        </picture>
+        <div class="wrapper">
+          <picture class="dessert__image">
+            <source media="(min-width:1024px)" srcset="${item.image.desktop}">
+            <source media="(min-width:760px)" srcset="${item.image.tablet}">
+            <img src="${item.image.mobile}">
+            </picture>
+            <button class="dessert__add-to-cart-btn">Add to Cart</button>
+        </div>
         <div class="dessert__info">
           <p class="dessert__category">${dessertCategory}</p>
           <h3 class="dessert__name">${dessertName}</h3>
           <p class="dessert__price">$${dessertPrice}</p>
         </div>
-        <button class="dessert__add-to-cart-btn">Add to Cart</button>
       `
 
       listOfDesserts.appendChild(li)
