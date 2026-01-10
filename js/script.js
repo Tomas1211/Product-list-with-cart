3bf6ce31ba66358c9b000e7b42417e159acfd667
Tomáš Modrovský <modrovskytomas@gmail.com>
2026-01-10 08:39:12 +0100

    replacing button attributes from id to class

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ fetch("./data.json")
           <h3 class="dessert__name">${dessertName}</h3>
           <p class="dessert__price">${dessertPrice}</p>
         </div>
-        <button id="addToCartBtn">Add to Cart</button>
+        <button class="add-to-cart-btn">Add to Cart</button>
       `
 
       listOfDesserts.appendChild(li)
